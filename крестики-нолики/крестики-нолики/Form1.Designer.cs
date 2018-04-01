cb85e6666615dc9009992c5a00fdd7231041e5fd
Emin <30857069+eminbabayev@users.noreply.github.com>
2018-04-01 23:57:07 +0400

    C# Tic Tac
    Кре́стики-но́лики— логическая игра между двумя противниками на квадратном поле 3 на 3 клетки или бо́льшего размера (вплоть до «бесконечного поля»). Один из игроков играет «крестиками», второй — «ноликами». В традиционной китайской игре  используются черные и белые камни.
    
    Игроки по очереди ставят на свободные клетки поля 3х3 знаки (один всегда крестики, другой всегда нолики). Первый, выстроивший в ряд 3 своих фигуры по вертикали, горизонтали или диагонали, выигрывает. Первый ход делает игрок, ставящий крестики.
    
    Обычно по завершении партии выигравшая сторона зачёркивает чертой свои три знака (нолика или крестика), составляющих сплошной ряд.

---
+++ крестики-нолики/крестики-нолики/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace крестики_нолики
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A00 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A01 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.NGButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.EButton = new System.Windows.Forms.Button();
+            this.XWin = new System.Windows.Forms.Label();
+            this.OWin = new System.Windows.Forms.Label();
+            this.Draws = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // A00
+            // 
+            this.A00.BackColor = System.Drawing.Color.FloralWhite;
+            this.A00.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A00.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A00.ForeColor = System.Drawing.Color.Purple;
+            this.A00.Location = new System.Drawing.Point(24, 20);
+            this.A00.Name = "A00";
+            this.A00.Size = new System.Drawing.Size(118, 87);
+            this.A00.TabIndex = 0;
+            this.A00.UseVisualStyleBackColor = false;
+            this.A00.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A10
+            // 
+            this.A10.BackColor = System.Drawing.Color.FloralWhite;
+            this.A10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A10.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic);
+            this.A10.ForeColor = System.Drawing.Color.Purple;
+            this.A10.Location = new System.Drawing.Point(24, 113);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(118, 87);
+            this.A10.TabIndex = 1;
+            this.A10.UseVisualStyleBackColor = false;
+            this.A10.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A20
+            // 
+            this.A20.BackColor = System.Drawing.Color.FloralWhite;
+            this.A20.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A20.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic);
+            this.A20.ForeColor = System.Drawing.Color.Purple;
+            this.A20.Location = new System.Drawing.Point(24, 206);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(118, 87);
+            this.A20.TabIndex = 2;
+            this.A20.UseVisualStyleBackColor = false;
+            this.A20.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A21
+            // 
+            this.A21.BackColor = System.Drawing.Color.FloralWhite;
+            this.A21.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A21.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic);
+            this.A21.ForeColor = System.Drawing.Color.Purple;
+            this.A21.Location = new System.Drawing.Point(158, 206);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(118, 87);
+            this.A21.TabIndex = 5;
+            this.A21.UseVisualStyleBackColor = false;
+            this.A21.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A11
+            // 
+            this.A11.BackColor = System.Drawing.Color.FloralWhite;
+            this.A11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A11.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic);
+            this.A11.ForeColor = System.Drawing.Color.Purple;
+            this.A11.Location = new System.Drawing.Point(158, 113);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(118, 87);
+            this.A11.TabIndex = 4;
+            this.A11.UseVisualStyleBackColor = false;
+            this.A11.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A01
+            // 
+            this.A01.BackColor = System.Drawing.Color.FloralWhite;
+            this.A01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A01.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic);
+            this.A01.ForeColor = System.Drawing.Color.Purple;
+            this.A01.Location = new System.Drawing.Point(158, 20);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(118, 87);
+            this.A01.TabIndex = 3;
+            this.A01.UseVisualStyleBackColor = false;
+            this.A01.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A22
+            // 
+            this.A22.BackColor = System.Drawing.Color.FloralWhite;
+            this.A22.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A22.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic);
+            this.A22.ForeColor = System.Drawing.Color.Purple;
+            this.A22.Location = new System.Drawing.Point(294, 206);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(118, 87);
+            this.A22.TabIndex = 8;
+            this.A22.UseVisualStyleBackColor = false;
+            this.A22.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A12
+            // 
+            this.A12.BackColor = System.Drawing.Color.FloralWhite;
+            this.A12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A12.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic);
+            this.A12.ForeColor = System.Drawing.Color.Purple;
+            this.A12.Location = new System.Drawing.Point(294, 113);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(118, 87);
+            this.A12.TabIndex = 7;
+            this.A12.UseVisualStyleBackColor = false;
+            this.A12.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A02
+            // 
+            this.A02.BackColor = System.Drawing.Color.FloralWhite;
+            this.A02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.A02.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Italic);
+            this.A02.ForeColor = System.Drawing.Color.Purple;
+            this.A02.Location = new System.Drawing.Point(294, 20);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(118, 87);
+            this.A02.TabIndex = 6;
+            this.A02.UseVisualStyleBackColor = false;
+            this.A02.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // NGButton
+            // 
+            this.NGButton.BackColor = System.Drawing.Color.DarkGray;
+            this.NGButton.Font = new System.Drawing.Font("A3 Arial AzCyr", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NGButton.Location = new System.Drawing.Point(24, 392);
+            this.NGButton.Name = "NGButton";
+            this.NGButton.Size = new System.Drawing.Size(118, 50);
+            this.NGButton.TabIndex = 9;
+            this.NGButton.Text = "New Game";
+            this.NGButton.UseVisualStyleBackColor = false;
+            this.NGButton.Click += new System.EventHandler(this.NGButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGray;
+            this.button1.Font = new System.Drawing.Font("A3 Arial AzCyr", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(170, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 50);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EButton
+            // 
+            this.EButton.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.EButton.Font = new System.Drawing.Font("A3 Arial AzCyr", 10.2F, System.Drawing.FontStyle.Bold);
+            this.EButton.Location = new System.Drawing.Point(503, 392);
+            this.EButton.Name = "EButton";
+            this.EButton.Size = new System.Drawing.Size(118, 50);
+            this.EButton.TabIndex = 11;
+            this.EButton.Text = "Exit";
+            this.EButton.UseVisualStyleBackColor = false;
+            this.EButton.Click += new System.EventHandler(this.EButton_Click);
+            // 
+            // XWin
+            // 
+            this.XWin.BackColor = System.Drawing.Color.Gainsboro;
+            this.XWin.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XWin.ForeColor = System.Drawing.Color.Maroon;
+            this.XWin.Location = new System.Drawing.Point(546, 45);
+            this.XWin.Name = "XWin";
+            this.XWin.Padding = new System.Windows.Forms.Padding(20, 20, 5, 0);
+            this.XWin.Size = new System.Drawing.Size(89, 62);
+            this.XWin.TabIndex = 12;
+            this.XWin.Text = "label1";
+            // 
+            // OWin
+            // 
+            this.OWin.BackColor = System.Drawing.Color.LightGray;
+            this.OWin.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OWin.ForeColor = System.Drawing.Color.Maroon;
+            this.OWin.Location = new System.Drawing.Point(546, 129);
+            this.OWin.Name = "OWin";
+            this.OWin.Padding = new System.Windows.Forms.Padding(20, 20, 0, 0);
+            this.OWin.Size = new System.Drawing.Size(89, 60);
+            this.OWin.TabIndex = 13;
+            this.OWin.Text = "label2";
+            // 
+            // Draws
+            // 
+            this.Draws.BackColor = System.Drawing.Color.LightGray;
+            this.Draws.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Draws.ForeColor = System.Drawing.Color.Maroon;
+            this.Draws.Location = new System.Drawing.Point(546, 206);
+            this.Draws.Name = "Draws";
+            this.Draws.Padding = new System.Windows.Forms.Padding(10, 20, 0, 0);
+            this.Draws.Size = new System.Drawing.Size(89, 65);
+            this.Draws.TabIndex = 14;
+            this.Draws.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::крестики_нолики.Properties.Resources._181269802;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(659, 454);
+            this.Controls.Add(this.Draws);
+            this.Controls.Add(this.OWin);
+            this.Controls.Add(this.XWin);
+            this.Controls.Add(this.EButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.NGButton);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A01);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A00);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Крестики-Нолики";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A00;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button NGButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button EButton;
+        private System.Windows.Forms.Label XWin;
+        private System.Windows.Forms.Label OWin;
+        private System.Windows.Forms.Label Draws;
+    }
+}
+
